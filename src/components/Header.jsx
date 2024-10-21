a20f496a0d66e110e1113e788e0cf936387f00b6
Chris Stafford <chrisgstafford@outlook.com>
2024-10-21 20:57:54 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,7 +68,7 @@ const Header = ({ isAuthenticated, handleLogin, handleLogout, openModal }) => {
 
         {/* Navigation Links */}
         <nav
-          className="flex-grow flex justify-right sm:justify-center space-x-3 sm:space-x-10"
+          className="flex-grow flex justify-end sm:justify-center space-x-3 sm:space-x-10"
           style={{ marginLeft: "-50px", marginTop: "0px" }}
         >
           {!isAuthenticated && (
